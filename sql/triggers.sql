c7bd46d437ef68b0bd0487bd9b792036bc0b126c
Pedro Fraga <antoniopedrofraga@gmail.com>
2016-04-11 05:00:52 +0100

    3 triggers done, 4 to go

--- sql/triggers.sql
+++ sql/triggers.sql
@@ -36,7 +36,7 @@ $$
   END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION atualizaMaiorLicitacao ( _idMembroBanido INTEGER )
+CREATE OR REPLACE FUNCTION atualizaMaiorLicitacao ()
  	RETURNS void AS
 $$
 	DECLARE
@@ -46,12 +46,13 @@ $$
 	BEGIN
 		FOR _leilao IN (SELECT idLeilao FROM Leilao)
   		LOOP
-    		UPDATE Leilao SET licitacaoAtual = (SELECT licitacaoAtual FROM Licitacao WHERE idLeilao = _leilao ORDER BY valor DESC LIMIT 1) 
+    		UPDATE Leilao SET licitacaoAtual = (SELECT valor FROM Licitacao WHERE idLeilao = _leilao ORDER BY valor DESC LIMIT 1) 
     		WHERE idLeilao = _leilao RETURNING nome INTO _nomeLeilao;
-    		/*IF (SELECT idCliente FROM Licitacao WHERE idLeilao = _leilao ORDER BY valor DESC LIMIT 1) IS NOT NULL THEN 
-    			INSERT INTO Notificacao (idUtilizador, texto) VALUES ((SELECT idCliente FROM Licitacao WHERE idLeilao = _leilao ORDER BY valor DESC LIMIT 1)
-    				, 'A sua licitacao no leilao ' || _nomeLeilao || ' voltou a ser a mais alta.');
-    		END IF;*/
+
+    		SELECT idCliente INTO _cliente FROM Licitacao WHERE idLeilao = _leilao ORDER BY valor DESC LIMIT 1;
+    		IF (_cliente) IS NOT NULL THEN 
+    			INSERT INTO Notificacao (idUtilizador, texto) VALUES ( _cliente, 'A sua licitacao no leilao ' || _nomeLeilao || ' voltou a ser a mais alta.');
+    		END IF;
   		END LOOP;
 	END;
 $$ LANGUAGE plpgsql;
@@ -61,13 +62,13 @@ CREATE OR REPLACE FUNCTION membroBanido ()
 $$
 	BEGIN
 		-- Leiloeiro banido
-		EXECUTE removeLicitacoesENotifica( NEW.idMembroBanido );
+		EXECUTE removeLicitacoesENotifica ( NEW.idMembroBanido );
 		DELETE FROM Leilao
 			WHERE idLeiloeiro = NEW.idMembroBanido;
 		-- Cliente banido
 		DELETE FROM Licitacao 
 			WHERE idCliente = NEW.idMembroBanido;
-		EXECUTE atualizaMaiorLicitacao ( NEW.idMembroBanido );
+		EXECUTE atualizaMaiorLicitacao ();
 		RETURN NEW;
 	END;
 $$ LANGUAGE plpgsql;
